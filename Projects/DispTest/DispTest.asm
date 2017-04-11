748d4666fa593cf650b436769dd6fabb432effd3
anotherstudent <dr.enter256@gmail.com>
2017-04-11 19:04:28 +0300

    add dosbox

--- Projects/DispTest/DispTest.asm
+++ Projects/DispTest/DispTest.asm
@@ -4,20 +4,20 @@
 locals
 
 .const
-	screenWidth = 320
-	screenHeight = 200
-	screenSize = (screenWidth*screenHeight)
+    screenWidth = 320
+    screenHeight = 200
+    screenSize = (screenWidth*screenHeight)
 
 .data
-	sRedScreen equ 'Red screen'
-	sGreenScreen equ 'Green screen'
-	sBlueScreen equ 'Blue screen'
-	sWhiteScreen equ 'White screen'
-	sHorizontalLines equ 'Horizontal lines'
-	sVerticalLines equ 'Vertical lines'
-	sGrayGradient equ 'Gray gradient'
-	sColorGradient equ 'Color gradient'
-	    
+    sRedScreen equ 'Red screen'
+    sGreenScreen equ 'Green screen'
+    sBlueScreen equ 'Blue screen'
+    sWhiteScreen equ 'White screen'
+    sHorizontalLines equ 'Horizontal lines'
+    sVerticalLines equ 'Vertical lines'
+    sGrayGradient equ 'Gray gradient'
+    sColorGradient equ 'Color gradient'
+
     menuText db\
 '+----------------------+',13,10,\
 '|      Screen Test     |',13,10,\
@@ -34,23 +34,23 @@ locals
 '7) ',sGrayGradient,13,10,\
 '8) ',sColorGradient,13,10,\
 'E) Exit',13,10,'$'
-	
-	redScreenText db sRedScreen,13,10,'$'
-	greenScreenText db sGreenScreen,13,10,'$'
-	blueScreenText db sBlueScreen,13,10,'$'
-	whiteScreenText db sWhiteScreen,13,10,'$'
-	horizontalLinesText db sHorizontalLines,13,10,'$'
-	verticalLinesText db sVerticalLines,13,10,'$'
-	grayGradientText db sGrayGradient,13,10,'$'
-	colorGradientText db sColorGradient,13,10,'$'
-	
-	exitText db 13,10,'Press [esc] to exit','$'
-	
+
+    redScreenText db sRedScreen,13,10,'$'
+    greenScreenText db sGreenScreen,13,10,'$'
+    blueScreenText db sBlueScreen,13,10,'$'
+    whiteScreenText db sWhiteScreen,13,10,'$'
+    horizontalLinesText db sHorizontalLines,13,10,'$'
+    verticalLinesText db sVerticalLines,13,10,'$'
+    grayGradientText db sGrayGradient,13,10,'$'
+    colorGradientText db sColorGradient,13,10,'$'
+
+    exitText db 13,10,'Press [esc] to exit','$'
+
 .code
 
 ; clear screen: [color]
 cls:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2+2; ret + color arg
@@ -62,10 +62,10 @@ cls:
     mov ax,[bp]; load [color] to ax
     mov di,0; 0 to di
 @@loop:
-	mov es:byte ptr[di],al; save byte to video ram
-	inc di
-	cmp di,screenSize
-	jnz @@loop; if di < screenSize then goto loop
+    mov es:byte ptr[di],al; save byte to video ram
+    inc di
+    cmp di,screenSize
+    jnz @@loop; if di < screenSize then goto loop
     ; restore registers
     pop di
     pop cx
@@ -76,7 +76,7 @@ cls:
 
 ; print test name on screen: [@text]
 printName:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2+2; ret + text arg
@@ -116,14 +116,14 @@ printName:
     ret 2
     
 redScreen:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2; ret
     ; print name
-	push offset redScreenText
-	call printName
-	; test
+    push offset redScreenText
+    call printName
+    ; test
     push 40; red color in std palette
     call cls
     ; return
@@ -131,14 +131,14 @@ redScreen:
     ret
     
 greenScreen:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2; ret
     ; print name
-	push offset greenScreenText
-	call printName
-	; test
+    push offset greenScreenText
+    call printName
+    ; test
     push 48; green color in std palette
     call cls
     ; return
@@ -146,14 +146,14 @@ greenScreen:
     ret
     
 blueScreen:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2; ret
     ; print name
-	push offset blueScreenText
-	call printName
-	; test
+    push offset blueScreenText
+    call printName
+    ; test
     push 32; blue color in std palette
     call cls
     ; return
@@ -161,14 +161,14 @@ blueScreen:
     ret
     
 whiteScreen:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2; ret
     ; print name
-	push offset whiteScreenText
-	call printName
-	; test
+    push offset whiteScreenText
+    call printName
+    ; test
     push 15; white color in std palette
     call cls
     ; return
@@ -176,7 +176,7 @@ whiteScreen:
     ret
    
 horizontalLines:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2
@@ -187,72 +187,72 @@ horizontalLines:
     push di
     push dx
     ; print name
-	push offset horizontalLinesText
-	call printName
-	; clear screen
+    push offset horizontalLinesText
+    call printName
+    ; clear screen
     push 0; black color
     call cls
     ; draw
     ; y loop
     mov cx,0
 @@LoopY:
-	; get offset for y -> di
-	mov ax,cx
-	mov dx,screenWidth
-	mul dx
-	mov di,ax
-	; x loop
+    ; get offset for y -> di
+    mov ax,cx
+    mov dx,screenWidth
+    mul dx
+    mov di,ax
+    ; x loop
     mov bx,0
 @@LoopX:
-	; draw point
-	mov es:byte ptr[di+bx],15
-	inc bx
-	cmp bx,screenWidth
-	jnz @@LoopX
-	; end x loop
-	add cx,8
-	cmp cx,screenHeight
-	jl @@LoopY
-	; end y loop
-	; load registers
-	pop dx
-	pop di
-	pop cx
-	pop bx
-	pop ax
+    ; draw point
+    mov es:byte ptr[di+bx],15
+    inc bx
+    cmp bx,screenWidth
+    jnz @@LoopX
+    ; end x loop
+    add cx,8
+    cmp cx,screenHeight
+    jl @@LoopY
+    ; end y loop
+    ; load registers
+    pop dx
+    pop di
+    pop cx
+    pop bx
+    pop ax
     ; return
     pop bp; load bp
     ret
     
 vertiacalLines:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2
     ; save registers
     push di
     ; print name
-	push offset verticalLinesText
-	call printName
-	; clear screen
+    push offset verticalLinesText
+    call printName
+    ; clear screen
     push 0; black color
     call cls
     ; draw
     mov di,0
 @@Loop:
-	; draw point
-	mov es:byte ptr[di],15
-	add di,8
-	cmp di,screenSize
-	jbe @@Loop
+    ; draw point
+    mov es:byte ptr[di],15
+    add di,8
+    cmp di,screenSize
+    jbe @@Loop
     ; load registers
-	pop di
+    pop di
     ; return
     pop bp; load bp
     ret
     
 grayGradient:
-	; enter
+ ; enter
     push bp; save bp
     mov bp,sp
     add bp,2
@@ -263,44 +263,44 @@ grayGradient:
     push dx
     push di
     ; print name
-	push offset grayGradientText
-	call printName
+    push offset grayGradientText
+    call printName
     ; draw
     mov di,0
     ; y loop
     mov di,0
 @@LoopY:
-	; x loop
+    ; x loop
     mov bx,0
 @@LoopX:
-	; get offset [0..15] -> al
-	mov ax,bx
-	mov dx,20
-	div dl
-	; al + 16 -> al - gray colors offset in std color table
-	add al,16
-	; draw point
-	mov es:byte ptr[di+bx],al
-	inc bx
-	cmp bx,screenWidth
-	jnz @@LoopX
-	; end x loop
-	add di,screenWidth
-	cmp di,screenSize
-	jbe @@LoopY
-	; end y loop
-	; load registers
-	pop di
-	pop dx
-	pop cx
-	pop bx
-	pop ax
-	; return
+    ; get offset [0..15] -> al
+    mov ax,bx
+    mov dx,20
+    div dl
+    ; al + 16 -> al - gray colors offset in std color table
+    add al,16
+    ; draw point
+    mov es:byte ptr[di+bx],al
+    inc bx
+    cmp bx,screenWidth
+    jnz @@LoopX
+    ; end x loop
+    add di,screenWidth
+    cmp di,screenSize
+    jbe @@LoopY
+    ; end y loop
+    ; load registers
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ; return
     pop bp; load bp
     ret
     
 colorGradient:
-	; enter
+    ; enter
     push bp; save bp
     mov bp,sp
     add bp,2
@@ -311,57 +311,57 @@ colorGradient:
     push dx
     push di
     ; print name
-	push offset grayGradientText
-	call printName
+    push offset colorGradientText
+    call printName
     ; draw
     mov di,0
     ; y loop
     mov di,0
 @@LoopY:
-	; x loop
+    ; x loop
     mov bx,0
 @@LoopX:
-	; get offset [0..15] -> al
-	mov ax,bx
-	mov dx,20
-	div dl
-	; draw point
-	mov es:byte ptr[di+bx],al
-	inc bx
-	cmp bx,screenWidth
-	jnz @@LoopX
-	; end x loop
-	add di,screenWidth
-	cmp di,screenSize
-	jbe @@LoopY
-	; end y loop
-	; load registers
-	pop di
-	pop dx
-	pop cx
-	pop bx
-	pop ax
-	; return
+    ; get offset [0..15] -> al
+    mov ax,bx
+    mov dx,20
+    div dl
+    ; draw point
+    mov es:byte ptr[di+bx],al
+    inc bx
+    cmp bx,screenWidth
+    jnz @@LoopX
+    ; end x loop
+    add di,screenWidth
+    cmp di,screenSize
+    jbe @@LoopY
+    ; end y loop
+    ; load registers
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ; return
     pop bp; load bp
     ret
         
 .data
-	testProc dw redScreen, greenScreen, blueScreen, whiteScreen,\
-	horizontalLines, vertiacalLines, grayGradient, colorGradient
+    testProc dw redScreen, greenScreen, blueScreen, whiteScreen,\
+    horizontalLines, vertiacalLines, grayGradient, colorGradient
 
 .code
 
 ; enter point
 .startup
-	; std setup
+    ; std setup
     mov ax,@data
     mov ds,ax
 
-	; screen ram -> es
-	mov ax,0a000h
-	mov es,ax
+    ; screen ram -> es
+    mov ax,0a000h
+    mov es,ax
 
-	; set video mode 10
+    ; set video mode 10
     mov ax, 13h
     int 10h    
     
@@ -373,64 +373,64 @@ colorGradient:
     mov ah,9
     mov dx,offset menuText
     int 21h
-   	; getkey
-	mov ah,08h
-	int 21h
-	
-	; exit
-	cmp al,'e'
-	jz @@exit
-	
-	; select screen test proc
-	cmp al,'1'
-	jl @@endCase
-	cmp al,'8'
-	jg @@endCase
-	; al - '0' -> ax
-	mov ah,'1'
-	sub al,ah
-	mov ah,0
-	mov cx,ax
+    ; getkey
+    mov ah,08h
+    int 21h
+    
+    ; exit
+    cmp al,'e'
+    jz @@exit
+    
+    ; select screen test proc
+    cmp al,'1'
+    jl @@endCase
+    cmp al,'8'
+    jg @@endCase
+    ; al - '0' -> ax
+    mov ah,'1'
+    sub al,ah
+    mov ah,0
+    mov cx,ax
 @@run:
-	; ax * 2 -> bx
-	mov dl,2
-	mul dl
-	mov bx,ax
-	; []
-	mov bx,[bx+testProc]
-	; call
-	call bx
+    ; ax * 2 -> bx
+    mov dl,2
+    mul dl
+    mov bx,ax
+    ; []
+    mov bx,[bx+testProc]
+    ; call
+    call bx
 
 @@getKey:
-   	; getkey
-	mov ah,08h
-	int 21h
-	; ex keys
-	cmp al,0
-	jnz @@endCase
-	mov ah,08h
-	int 21h
+    ; getkey
+    mov ah,08h
+    int 21h
+    ; ex keys
+    cmp al,0
+    jnz @@endCase
+    mov ah,08h
+    int 21h
     ; ->
-	cmp al,77
-	jnz @@test2
-	cmp cx,7
-	jz @@getKey	
-	inc cx
-	mov ax,cx
-	jmp @@run
+    cmp al,77
+    jnz @@test2
+    cmp cx,7
+    jz @@getKey	
+    inc cx
+    mov ax,cx
+    jmp @@run
 @@test2:
-	; <-
-	cmp al,75
-	jnz @@endCase
-	cmp cx,0
-	jz @@getKey	
-	dec cx
-	mov ax,cx
-	jmp @@run
+    ; <-
+    cmp al,75
+    jnz @@endCase
+    cmp cx,0
+    jz @@getKey	
+    dec cx
+    mov ax,cx
+    jmp @@run
 
 @@endCase:
-	jmp @@loop
-	
+    jmp @@loop
+    
 @@exit:
     ;set text mode
     mov ax, 2h;
--- Projects/WriteLib/clear.bat
+++ Projects/WriteLib/clear.bat
@@ -3,4 +3,4 @@ if exist *.obj del *.obj
 if exist *.map del *.map
 if exist *.td del *.td
 if exist *.tr del *.tr
-if exist %name%.exe del %name%.exe
+rem if exist %name%.exe del %name%.exe
